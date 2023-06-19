5217925307399a78bcaa90a94a284e567424293d
ivan-cardenas <ilcardenas26@gmail.com>
2023-06-19 15:20:19 +0200

    Adjusted to folder

--- assets/graphs.js
+++
@@ -1,7 +0,0 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
-
-const data = [
-    {"HabitatTyp": "Brush/Scrub-Shrub", value: 117.588},
-    
-]
