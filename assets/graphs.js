9bc63972ab3e1ccff1a608f53e594c7acdd64c58
ivan-cardenas <ilcardenas26@gmail.com>
2023-06-19 15:00:11 +0200

    Upload Map Files
    Initial Setup and upload files

---
+++ assets/graphs.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {"HabitatTyp": "Brush/Scrub-Shrub", value: 117.588},
+    
+]
